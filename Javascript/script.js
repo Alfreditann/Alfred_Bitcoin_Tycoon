b908ae29efd1055e52658aa97cb53a4663f19b49
Alfreditann <alula006@osloskolen.no>
2025-02-27 11:21:48 +0100

    27 feb
    jeg har lagt til autobitcoin

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,15 +1,18 @@
 
 let antallBitcoin = 0
 let bitcoinEarn = 1
-let minusBitcoin = 10
+let minusKroner = 10
+let antallKroner = 0
 
-function updateUI(){
+function updateUI() {
     document.getElementById("viserAntallBitcoin").innerText = antallBitcoin;
+    document.getElementById("viserAntallKroner").innerText = antallKroner
 }
 
+
 function merBitcoin() {
-    if (antallBitcoin >= minusBitcoin) {
-        antallBitcoin -= minusBitcoin;
+    if (antallKroner >= minusKroner) {
+        antallKroner -= minusKroner;
         minusBitcoin *= 2;
         bitcoinEarn = bitcoinEarn + 1;
         updateUI();
@@ -17,16 +20,38 @@ function merBitcoin() {
     else {
         bitcoinEarn = bitcoinEarn;
         updateUI();
+        alert("Du er fattig 😂😂😂😂")
     }
 }
 function counter() {
-        antallBitcoin += bitcoinEarn
-        updateUI();
-        
-    }
-        
-function telefonEarnings() {
+    antallBitcoin += bitcoinEarn
+    updateUI();
+
 }
 
+function selleBitcoin() {
+    if (antallBitcoin >= 1) {
+        antallBitcoin = antallBitcoin - 1;
+        antallKroner = antallKroner + 1;
+        updateUI();
+    }
+    else {
+        alert("du har ikke bitcoin å selge")
+    }
+}
+function telefonEarning() {
+    if (antallKroner >= 10) {
+        setInterval(autoBitcoin, 1000
+        );
+        function autoBitcoin() {
+            document.getElementById("viserAntallBitcoin").innerHTML = antallBitcoin = antallBitcoin + 1;
+        }
+        antallKroner -= 10;
+        updateUI();
+    }
+    else {
+        alert("get your money up lil bro")
+    }
+}
 
 updateUI();
