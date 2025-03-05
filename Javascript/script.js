c78fa06a1d931d6d1d92b0a8cffdced80138251a
Alfreditann <alula006@osloskolen.no>
2025-03-05 11:26:28 +0100

    5 mars

--- Javascript/script.js
+++ Javascript/script.js
@@ -3,7 +3,7 @@ let antallBitcoin = 0
 let bitcoinEarn = 1
 let minusKroner = 10
 let antallKroner = 0
-let antallStrøm = 0
+let antallStrom = 0
 let hasBougthTelefon = false;
 let hasBougthIpad = false;
 let hasBougthLaptop = false;
@@ -13,9 +13,9 @@ let hasBougthNasa = false;
 function updateUI() {
     document.getElementById("viserAntallBitcoin").innerText = antallBitcoin;
     document.getElementById("viserAntallKroner").innerText = antallKroner
-    document.getElementById("viserAntallStrøm").innerText = antallStrøm
+    document.getElementById("viserAntallStrøm").innerText = antallStrom
 
-     while(hasBougthTelefon == true && antallStrøm >=1) {
+     while(hasBougthTelefon == true && antallStrom >=1) {
     
         setInterval(autoBitcoin, 1000)
         updateUI();
@@ -72,35 +72,18 @@ function selleTiKBitcoin() {
 function autoBitcoin() {
     
     if (!hasBougthTelefon && antallKroner >= 10) {
-        if(antallStrøm >= 0) { 
+        if(antallStrom >= 0) { 
         setInterval(autoBitcoin, 1000);
-        // function autoBitcoin() {
-        //     document.getElementById("viserAntallBitcoin").innerHTML = antallBitcoin = antallBitcoin + 1;
-        // }
         }
         antallKroner -= 10;
         hasBougthTelefon = true;
         updateUI();
     }
-    // else {
-    //     alert("get your money up lil bro")
-    // }
-console.log("sdffs")
    
 
 }
 
-// if (!hasBougthTelefon && antallKroner >= 10) {
-//     antallKroner -= 10;
-//     hasBougthTelefon = true;
-//     setInterval(autoBitcoin, 1000);
-//     updateUI();
-//     if (antallStrøm >= 1) {
-//             document.getElementById("viserAntallBitcoin").innerHTML = antallBitcoin = antallBitcoin + 1;
-//             antallStrøm = antallStrøm -1;
-//             updateUI();
-//     }
-// }
+
 function ipadEarning() {
     if (hasBougthIpad && antallKroner >= 100) {
         setInterval(autoBitcoin, 500);
@@ -161,7 +144,7 @@ function nasaEarning() {
 }
 function bGenerator() {
     if (antallKroner >= 10) {
-        antallStrøm = antallStrøm + 10
+        antallStrom = antallStrom + 10
         antallKroner = antallKroner - 10;
         updateUI()   
     }
